4b6d406d57077fd17988499d1f6dbed9e7a0619d
Dewey Dunnington <dewey@fishandwhistle.net>
2017-01-03 17:56:35 -0500

    add plot build stub

---
+++ ggpy/plot_build.py
@@ -0,0 +1,14 @@
+
+
+class BuiltGGPlot(object):
+
+    def __init__(self, ggplot):
+        self.spec = ggplot.clone()
+
+    def build(self):
+        # todo: method stub
+        pass
+
+    def render(self, renderer):
+        # todo: method stub
+        pass
