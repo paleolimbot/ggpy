1d8a54b67be4a495c9563d7931f99b8c929ac9dd
Dewey Dunnington <dewey@fishandwhistle.net>
2017-01-10 21:43:21 -0400

    update plot interface

--- ggpy/_interface.py
+++ ggpy/_interface.py
@@ -1,4 +1,16 @@
 
+# all imported names must be protected by _ to avoid importing to the interface
+from .layer import Layer as _Layer
+from .geom_point import GeomPoint as _GeomPoint
+from .labels import labs as _labs
 
 # todo: this should store the shortcut functions that R uses (i.e. geom_point(), stat_bin())
-# will all be imported to ggpy namespace
+# will all be imported to ggpy namespace and will be available by ggplot().XXX
+
+
+def geom_point(mapping=None, inherit_aes=True):
+    return _Layer(mapping=mapping, geom=_GeomPoint(), inherit_aes=inherit_aes)
+
+
+def labs(**kwargs):
+    return _labs(**kwargs)
--- ggpy/plot.py
+++ ggpy/plot.py
@@ -9,6 +9,13 @@ from .coord_cartesian import CoordCartesian
 from .theme import Theme
 from .labels import make_labels, labs, Labels
 from .plot_build import BuiltGGPlot
+from . import _interface as interface
+
+
+# this function allows regular objects/modules to function as interfaces by filtering out only their methods
+# and skipping protected methods that may provide confusing functionality. The None object can be used as interface
+def _interface_dir(interface_obj):
+    return [obj for obj in dir(interface_obj) if not obj.startswith("_") and callable(getattr(interface_obj, obj))]
 
 
 class ggplot(object):
@@ -26,92 +33,107 @@ class ggplot(object):
         self._local_vars = local_vars if local_vars is not None else {}  # todo: don't know if these are needed yet
         self._global_vars = global_vars if global_vars is not None else {}
 
-    def layer(self, layer=None, **kwargs):
+        # this is only in the Python edition (self._built must be set to None whenever this object
+        # is modified
+        self._built = None
+        self._interface = interface
+
+    def __layer(self, layer=None, **kwargs):
         if layer is None:
             self._layers.append(Layer(**kwargs))
+            self._built = None
         elif callable(layer):
             newlayer = layer(**kwargs)
             if not isinstance(newlayer, Layer):
                 raise TypeError("layer must return value of type Layer")
             self._layers.append(newlayer)
+            self._built = None
         elif isinstance(layer, Layer):
             if kwargs:
                 raise ValueError("layer is not callable, kwargs were ignored")
             self._layers.append(layer)
+            self._built = None
         else:
             raise TypeError("Layer to be added must be of type Layer")
         return self
 
-    def theme(self, theme=None, **kwargs):
+    def __theme(self, theme=None, **kwargs):
         if theme is None:
             self._theme = self._theme + Theme(**kwargs)
+            self._built = None
         elif callable(theme):
             newtheme = self._theme + theme(**kwargs)
             if not isinstance(newtheme, Theme):
                 raise TypeError("theme must return value of type Theme")
             self._theme = newtheme
+            self._built = None
         elif isinstance(theme, Theme):
             if kwargs:
                 raise ValueError("theme is not callable, kwargs were ignored")
             self._theme = self._theme + theme
+            self._built = None
         else:
             raise TypeError("Theme to be added must be of type Theme")
         return self
 
-    def coord(self, coord=None, **kwargs):
+    def __coord(self, coord=None, **kwargs):
         if coord is None:
             self._coordinates = CoordCartesian(**kwargs)
+            self._built = None
         elif callable(coord):
             newcoord = coord(**kwargs)
             if not isinstance(coord, Coord):
                 raise TypeError("coord must return value of type Coord")
             self._coordinates = newcoord
+            self._built = None
         elif isinstance(coord, Coord):
             if kwargs:
                 raise ValueError("coord is not callable, kwargs were ignored")
             self._coordinates = coord
+            self._built = None
         else:
             raise TypeError("Coordinates object must be of type Coord")
         return self
 
-    def facet(self, facet, **kwargs):
+    def __facet(self, facet, **kwargs):
         if facet == "null":
             self._facet = FacetNull(**kwargs)
+            self._built = None
         elif facet is None:
             self._facet = FacetNull(**kwargs)
+            self._built = None
         elif callable(facet):
             newfacet = facet(**kwargs)
             if not isinstance(facet, Facet):
                 raise TypeError("facet must return value of type Facet")
             self._facet = newfacet
+            self._built = None
         elif isinstance(facet, Facet):
             if kwargs:
                 raise ValueError("facet is not callable, kwargs were ignored")
             self._facet = facet
+            self._built = None
         else:
             raise TypeError("facet must be of type Facet or one of 'null', 'wrap', or 'grid'")
         return self
 
-    def scale(self, scale, **kwargs):
+    def __scale(self, scale, **kwargs):
         if callable(scale):
             newscale = scale(**kwargs)
             if not isinstance(newscale, Scale):
                 raise TypeError("Scale function must return object of type Scale")
             self._scales.add(newscale)
+            self._built = None
         elif isinstance(scale, Scale):
             if kwargs:
                 raise ValueError("scale is not callable, kwargs were ignored")
             self._scales.add(scale)
+            self._built = None
         else:
             raise TypeError("scale must be of type Scale")
         return self
 
-    def scales(self, *scales, **kwargs):
-        for scale in scales:
-            self.scale(scale, **kwargs)
-        return self
-
-    def labs(self, **kwargs):
+    def __labs(self, **kwargs):
         self._labels = self._labels + labs(**kwargs)
         return self
 
@@ -126,34 +148,62 @@ class ggplot(object):
         newobj._labels = self._labels.copy()
         return newobj
 
-    def __iadd__(self, other):
+    def modify(self, other):
         if isinstance(other, list):
             for obj in other:
                 self.__iadd__(obj)
         elif isinstance(other, Scale):
-            self.scale(other)
+            self.__scale(other)
         elif isinstance(other, Layer):
-            self.layer(other)
+            self.__layer(other)
         elif isinstance(other, Coord):
-            self.coord(other)
+            self.__coord(other)
         elif isinstance(other, Labels):
-            self.labs(**other)
+            self.__labs(**other)
         elif isinstance(other, Theme):
-            self.theme(other)
+            self.__theme(other)
         elif isinstance(other, Facet):
-            self.facet(other)
+            self.__facet(other)
         else:
             raise TypeError("Don't know how to add type %s to ggplot" % type(other).__name__)
         return self
+
+    def add(self, other):
+        newobj = self.clone()
+        return newobj.modify(other)
+
+    def build(self):
+        if self._built is None:
+            self._built = BuiltGGPlot(self)
+        return self._built
+
+    def rebuild(self):
+        self._built = None
+        return self.build()
+
+    def __iadd__(self, other):
+        return self.add(other)
 
     def __add__(self, other):
         newobj = self.clone()
         if isinstance(other, list):
             for obj in other:
-                newobj.__iadd__(obj)
+                newobj.modify(obj)
         else:
-            newobj.__iadd__(other)
+            newobj.modify(other)
         return newobj
 
-    def build(self):
-        return BuiltGGPlot(self)
+    # this allows stringing together functions imported to the _interface module with
+    # the dot selector
+    def __getattr__(self, item):
+        if item in object.__dir__(self):
+            return object.__getattribute__(self, item)
+        elif self._interface is not None and item in _interface_dir(self._interface):
+            intmeth = getattr(self._interface, item)
+            return lambda *args, **kwargs: self.add(intmeth(*args, **kwargs))
+        raise AttributeError("'%s' has no attribute '%s'" % (type(self).__name__, item))
+
+    def __dir__(self):
+        return list(object.__dir__(self)) + list(_interface_dir(self._interface))
+
+
